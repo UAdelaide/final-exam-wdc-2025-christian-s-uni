699af60241068b2e0ff430cf828889bea6a45c2e
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 05:08:52 +0000

    [automatic commit] 2025-06-20 05:08:52 UTC 58.174.82.240

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -41,7 +41,7 @@ router.get('/walkers/summary', async function (req,res,next) {
     }
     const walkerReviews = await Promise.all(fetchReviews);
     for (let w = 0; w < walkerReviews.length; w++) {
-      for (let r = 0, r < walkerReviews[w].length, r++) {
+      for (let r = 0; r < walkerReviews[w].length; r++) {
         
       }
     }
