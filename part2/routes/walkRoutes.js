f832737cf96f3280178668474c3321f9d3f99cea
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 16:30:52 +0000

    [automatic commit] 2025-06-20 16:30:51 UTC 58.174.82.240

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -61,6 +61,7 @@ router.post('/:id/apply', async (req, res) => {
 
 // get all dogs from signed in user
 router.get('/getUserDogs', async (req,res) => {
+  try {
   // db.query to SELECT * FROM Dogs WHERE owner_id = ...
   var query = "SELECT * FROM Dogs WHERE owner_id = ?";
   var [dogs] = await db.query(query, [req.session.user.userid]);
