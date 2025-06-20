4775dfd6d0c0aef6743bd7f77675ba490a08f04d
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 03:36:33 +0000

    [automatic commit] 2025-06-20 03:36:33 UTC 58.174.82.240

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,10 +1,13 @@
 var express = require('express');
+const db = require('../db');
 var router = express.Router();
 db = require('../db.js');
 
 /* GET home page. */
 router.get('/dogs', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  try {
+    const [rows] = db.query(SELECT name as dog_name, size, username as owner_username FROM Dogs INNER JOIN Users on Dogs.owner_id = Users.user_id;)
+  }
 });
 
 module.exports = router;
