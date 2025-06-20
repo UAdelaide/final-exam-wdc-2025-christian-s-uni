a1323d4a51281b971945501b56528ccf99db7fb5
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 03:18:54 +0000

    [automatic commit] 2025-06-20 03:18:54 UTC 58.174.82.240

--- part1/app.js
+++ part1/app.js
@@ -31,7 +31,27 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count < 1) {
         await db.execute(`
-            
+            INSERT INTO Users(username, email, password_hash, role) VALUES
+            ("alice123", "alice@example.com", "hashed123", 'owner'),
+            ("bobwalker", "bob@example.com", "hashed456", 'walker'),
+            ("carol123", "carol@example.com", "hashed789", 'owner'),
+            ("ben", "ben@example.com", "verysecure", 'owner'),
+            ("kenny06", "kenny@kennison.com", "evenmoresecure151", 'walker');
+
+
+            INSERT INTO Dogs(owner_id, name, size) VALUES
+            ((SELECT user_id FROM Users WHERE username = "alice123"), 'Max', 'medium'),
+            ((SELECT user_id FROM Users WHERE username = "carol123"), 'Bella', 'small'),
+            ((SELECT user_id FROM Users WHERE username = "ben"), "Clifford", 'large'),
+            ((SELECT user_id FROM Users WHERE username = "alice123"), 'Sam', 'small'),
+            ((SELECT user_id FROM Users WHERE username = "carol123"), 'Lucy', 'large');
+
+            INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
+            ((SELECT dog_id FROM Dogs WHERE name = "Max"), '2025-06-10 08:00:00', 30, "Parklands", 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Bella"), '2025-06-10 09:30:00', 45, "Beachside Ave", 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Clifford"), '2025-06-13 10:30:00', 60, "Croydon", 'completed'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Sam"), '2025-06-13 11:30:00', 15, "Rundle Mall", 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Lucy"), '2025-06-13 10:30:00', 30, "Semaphore", 'open');
             `)
     }
     } catch (err) {
