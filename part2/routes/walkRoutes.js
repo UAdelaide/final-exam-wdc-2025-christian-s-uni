df5f782e2dc78c5937afd6cb01c8b841894aa9e3
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 17:15:09 +0000

    [automatic commit] 2025-06-20 17:15:09 UTC 58.174.82.240

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -32,6 +32,7 @@ router.post('/', async (req, res) => {
     res.status(201).json({ message: 'Walk request created', request_id: result.insertId });
   } catch (error) {
     res.status(500).json({ error: 'Failed to create walk request' });
+    console.log(error);
   }
 });
 
