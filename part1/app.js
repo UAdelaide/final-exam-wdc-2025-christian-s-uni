6cb8a646321acf5ec87b32575b8377a14e504407
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 03:30:16 +0000

    [automatic commit] 2025-06-20 03:30:16 UTC 58.174.82.240

--- part1/app.js
+++ part1/app.js
@@ -27,12 +27,6 @@ app.use(cookieParser());
             ("carol123", "carol@example.com", "hashed789", 'owner'),
             ("ben", "ben@example.com", "verysecure", 'owner'),
             ("kenny06", "kenny@kennison.com", "evenmoresecure151", 'walker');
-            INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
-            ((SELECT dog_id FROM Dogs WHERE name = "Max"), '2025-06-10 08:00:00', 30, "Parklands", 'open'),
-            ((SELECT dog_id FROM Dogs WHERE name = "Bella"), '2025-06-10 09:30:00', 45, "Beachside Ave", 'accepted'),
-            ((SELECT dog_id FROM Dogs WHERE name = "Clifford"), '2025-06-13 10:30:00', 60, "Croydon", 'completed'),
-            ((SELECT dog_id FROM Dogs WHERE name = "Sam"), '2025-06-13 11:30:00', 15, "Rundle Mall", 'accepted'),
-            ((SELECT dog_id FROM Dogs WHERE name = "Lucy"), '2025-06-13 10:30:00', 30, "Semaphore", 'open');
             `);
             await db.execute(`INSERT INTO Dogs(owner_id, name, size) VALUES
             ((SELECT user_id FROM Users WHERE username = "alice123"), 'Max', 'medium'),
@@ -40,7 +34,12 @@ app.use(cookieParser());
             ((SELECT user_id FROM Users WHERE username = "ben"), "Clifford", 'large'),
             ((SELECT user_id FROM Users WHERE username = "alice123"), 'Sam', 'small'),
             ((SELECT user_id FROM Users WHERE username = "carol123"), 'Lucy', 'large');`);
-            
+            await db.execute(`INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
+            ((SELECT dog_id FROM Dogs WHERE name = "Max"), '2025-06-10 08:00:00', 30, "Parklands", 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Bella"), '2025-06-10 09:30:00', 45, "Beachside Ave", 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Clifford"), '2025-06-13 10:30:00', 60, "Croydon", 'completed'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Sam"), '2025-06-13 11:30:00', 15, "Rundle Mall", 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = "Lucy"), '2025-06-13 10:30:00', 30, "Semaphore", 'open');`);
             console.log('Successfully added test data.');
     } catch (err) {
     console.log('Error occurred, perhaps the data already exists?\n' + err);
