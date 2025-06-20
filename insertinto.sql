fe45120e1d3fa428ae9cf24dcf2a11ba9ceb73ee
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 02:02:28 +0000

    [automatic commit] 2025-06-20 02:02:28 UTC 58.174.82.240

--- insertinto.sql
+++ insertinto.sql
@@ -8,5 +8,7 @@ INSERT INTO Users(username, email, password_hash, role) VALUES
 
 INSERT INTO Dogs(owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "alice123"), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username = "carol123"), 'Bella', 'small')
-((SELECT user_id FROM Users WHERE username = "ben"), "Clifford", "large")
+((SELECT user_id FROM Users WHERE username = "carol123"), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username = "ben"), "Clifford", 'large')
+((SELECT user_id FROM Users WHERE username = "alice123"), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = "carol123"), 'Bella', 'small'),
