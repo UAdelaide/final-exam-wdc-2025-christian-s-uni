31d135c6c5b8b4549e2f34296621b916de2fe658
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 06:13:28 +0000

    [automatic commit] 2025-06-20 06:13:28 UTC 58.174.82.240

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const session = require('express-session');
 
 // Set up express-session
 
