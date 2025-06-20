86bfe6d3402b0d850cb989f2cd8412d5fef9aa49
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 05:49:02 +0000

    [automatic commit] 2025-06-20 05:49:01 UTC 58.174.82.240

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -12,5 +12,7 @@ createApp({
         var fetchedDogImg = await fetch("https://dog.ceo/api/breeds/image/random");
         var dogImgObject = await fetchedDogImg.json();
         this.dogImage = dogImgObject.message;
-    }
+    },
+    async 
+
 }).mount('#app');
