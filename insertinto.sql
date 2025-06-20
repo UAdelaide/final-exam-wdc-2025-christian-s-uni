38d969cb5d2ccc974e3376464b7810d252ee6d72
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 02:10:29 +0000

    [automatic commit] 2025-06-20 02:10:29 UTC 58.174.82.240

--- insertinto.sql
+++ insertinto.sql
@@ -16,4 +16,6 @@ INSERT INTO Dogs(owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "carol123"), 'Lucy', 'large');
 
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = "Max"), '2025-06-10 08:00:00', 30, "Parklands", 'open');
+((SELECT dog_id FROM Dogs WHERE name = "Max"), '2025-06-10 08:00:00', 30, "Parklands", 'open'),
+((SELECT dog_id FROM Dogs WHERE name = "Bella"), '2025-06-10 08:00:00', 30, "Parklands", 'open')
+;
