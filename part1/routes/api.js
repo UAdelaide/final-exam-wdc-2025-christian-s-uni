f71cf4756a3b15739a9e56b830b2539a28f5856e
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 04:12:34 +0000

    [automatic commit] 2025-06-20 04:12:34 UTC 58.174.82.240

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -32,7 +32,8 @@ router.get('/walkrequests/open', async function(req,res,next) {
 router.get('/walkers/summary', async function (req,res,next) {
   try {
     var query = `SELECT u.username AS walker_username,
-    COUNT(*) AS total_ratings
+    COUNT(*) AS total_ratings,
+    
     FROM WalkRatings wr
     INNER JOIN Users u ON wr.walker_id = u.user_id
     GROUP BY u.username`;
