44de1495a28097e3d437c27de08dbf3abb949fd3
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-20 05:40:10 +0000

    [automatic commit] 2025-06-20 05:40:09 UTC 58.174.82.240

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -3,8 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            dogName: "",
-            dogOwner: "",
+            dogName: "Ralph",
+            dogOwner: "Alice",
             dogImage: ""
         };
     },
