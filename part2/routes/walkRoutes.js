3568e21aa27005cc29d412a863613796ab579aaa
Christian Sabordo <a1942068@student.adelaide.edu.au>
2025-06-21 02:01:21 +0000

    [automatic commit] 2025-06-21 02:01:20 UTC 58.174.82.240

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -72,4 +72,18 @@ router.get('/userdogs', async (req,res) => {
   }
 });
 
+// Get all dogs in the database
+router.get('/dogs', async function(req, res, next) {
+  try {
+    var query = `SELECT name as dog_name, size, username as owner_username FROM Dogs
+    INNER JOIN Users
+    ON Dogs.owner_id = Users.user_id`;
+    const [rows] = await db.query(query);
+    return res.send(rows);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
 module.exports = router;
